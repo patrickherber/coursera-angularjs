66c92df8a4720d71e752692c9bddc74a470c8685
Patrick Herber <patrick.herber@gmail.com>
2016-10-10 21:19:26 +0200

    Assignment 4

--- assignment4/src/menudata.service.js
+++ assignment4/src/menudata.service.js
@@ -9,16 +9,16 @@ function MenuDataService($http) {
   var service = this;
 
   service.getAllCategories = function() {
-    return $http({url:'https://davids-restaurant.herokuapp.com/categories.json'}).then(function (response) {
+    return $http({url:'https://davids-restaurant.herokuapp.com/categories.json'}).then(function (result) {
       console.log(result.data);
-      return response.data;
+      return result.data;
     });
   };
 
   service.getItemsForCategory = function(categoryShortName) {
-    return $http({url:'https://davids-restaurant.herokuapp.com/menu_items.json?category=' + categoryShortName}).then(function (response) {
+    return $http({url:'https://davids-restaurant.herokuapp.com/menu_items.json?category=' + categoryShortName}).then(function (result) {
       console.log(result.data.menu_items);
-      return response.data.menu_items;
+      return result.data.menu_items;
     });
   };
 }
