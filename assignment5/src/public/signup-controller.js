81e36132473ef2d8ec2a837bac1da8a587729f60
Patrick Herber <patrick.herber@gmail.com>
2016-10-29 20:54:23 +0200

    Assignment 5

--- assignment5/src/public/signup-controller.js
+++ assignment5/src/public/signup-controller.js
@@ -13,11 +13,14 @@ function SignupController(InfoService,ApiPath) {
 
   ctrl.signup = function() {
     if (ctrl.surname && ctrl.email && ctrl.phoneMobile && ctrl.favoriteDish) {
+      var url = ApiPath + '/menu-items/' + ctrl.favoriteDish + '.json';
+      console.log(url);
       var promise = $http({
         method: 'GET',
-        url: (ApiPath + '/menu-items/' + ctrl.favoriteDish + '.json')
+        url: url
       });
       promise.then(function (response) {
+        console.log(response.data);
         service.saveInfo(ctrl.info);
         service.saveFavoriteDish(response.data);
         ctrl.info = angular.copy(service.getInfo());
